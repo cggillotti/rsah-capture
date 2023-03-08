16666c049b504b95d547f30768cb7b4786e55cdf
EC2 Default User <ec2-user@ip-172-31-64-64.ec2.internal>
2023-03-08 09:10:10 -0500

    File name change, gitignore add

--- rsah-controller.sh
+++ rsah-controller.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo $$ > /var/run/$1.pid
+echo $$ > /var/run/$2.pid
 
 echo "Starting Controller"
 START=$(/bin/date +%s)
 CURRENT=-1
 
 #target-time=7210
-MAXTIME=7210
-echo $$ > /var/run/$1.pid
+MAXTIME=$(($1 * 60))
+echo $$ > /var/run/$2.pid
 
-WORKER=$(cat /var/run/$1.pid)
+WORKER=$(cat /var/run/$2.pid)
 echo WORKER
 echo "Work spawn $WORKER"
 
 #sleep 10
 #exec "$@"
-$@ $1 & > /dev/null
+$2 $3 & > /dev/null
 TARGET="$!"
 echo "Spawned: $TARGET"
 while [ "$CURRENT" -lt "$MAXTIME" ]
